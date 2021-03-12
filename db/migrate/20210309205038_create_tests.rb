31643d16e98d4fd53bb852c87972c5b9a3e4ba37
Franerial <brugunt@gmail.com>
2021-03-13 02:16:04 +0300

    lesson_3 fix 2

--- db/migrate/20210309205038_create_tests.rb
+++ db/migrate/20210309205038_create_tests.rb
@@ -3,8 +3,8 @@ class CreateTests < ActiveRecord::Migration[6.1]
     create_table :tests do |t|
       t.string :title, limit: 50, null: false
       t.integer :level, default: 0
-      t.references(:category, index: true, foreign_key: true, null: false)
-      t.references(:user, index: true, foreign_key: true, null: false)
+      t.references :category, foreign_key: true, null: false
+      t.references :user, foreign_key: true, null: false
       t.timestamps
     end
   end
