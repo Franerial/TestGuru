88ab9d0d03bb2b89eca577d689256eeb2081d76a
Franerial <brugunt@gmail.com>
2021-03-17 01:09:09 +0300

    lesson 5

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,4 +2,8 @@ class Test < ApplicationRecord
   def self.tests_by_category(category_title)
     joins("JOIN categories ON tests.category_id = categories.id").where("categories.title = ?", category_title).order(id: :desc).pluck(:title)
   end
+  belongs_to :category
+  has_many :questions
+  has_many :users, through: :passing_tests
+  belongs_to :author, :class_name => "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   def tests_by_level(level)
     Test.joins("JOIN passing_tests ON tests.id = passing_tests.test_id").where("passing_tests.user_id = :user_id AND level = :level", { user_id: id, level: level })
   end
+
+  has_many :passing_tests
+  has_many :tests, through: :passing_tests
+  has_many :authored_tests, :foreign_key => "author_id", :class_name => "Test"
 end
