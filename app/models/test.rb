5dabde0dd3cde9aba22c82e391b955d5e5c241c6
Franerial <brugunt@gmail.com>
2021-03-18 23:51:28 +0300

    lesson 5 fix 2

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,10 +1,13 @@
 class Test < ApplicationRecord
-  def self.tests_by_category(category_title)
-    joins("JOIN categories ON tests.category_id = categories.id").where("categories.title = ?", category_title).order(id: :desc).pluck(:title)
-  end
   belongs_to :category
-  has_many :questions
-  has_many :passing_tests
-  has_many :users, through: :passing_tests
   belongs_to :author, :class_name => "User"
+
+  has_many :questions, dependent: :destroy
+  has_many :passing_tests, dependent: :destroy
+  has_many :users, through: :passing_tests
+
+  def self.tests_by_category(category_title)
+    #joins("JOIN categories ON tests.category_id = categories.id").where("categories.title = ?", category_title).order(id: :desc).pluck(:title)
+    joins(:category).where({ category: { title: category_title } }).order(id: :desc).pluck(:title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-  def tests_by_level(level)
-    Test.joins("JOIN passing_tests ON tests.id = passing_tests.test_id").where("passing_tests.user_id = :user_id AND level = :level", { user_id: id, level: level })
-  end
-
-  has_many :passing_tests
+  has_many :passing_tests, dependent: :destroy
   has_many :tests, through: :passing_tests
-  has_many :authored_tests, :foreign_key => "author_id", :class_name => "Test"
+  has_many :authored_tests, foreign_key: "author_id", class_name: "Test", dependent: :destroy
+
+  def tests_by_level(level)
+    #Test.joins("JOIN passing_tests ON tests.id = passing_tests.test_id").where("passing_tests.user_id = :user_id AND level = :level", { user_id: id, level: level })
+    Test.joins(:passing_tests).where({ passing_tests: { user_id: id }, level: level })
+  end
 end
