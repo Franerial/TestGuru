b295ad427da13eb2a8e69aa58b9346ac8e78eaf5
Franerial <brugunt@gmail.com>
2021-03-20 21:52:09 +0300

    lesson 6 (add scopes and validations)

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
+  default_scope { order(:title) }
   has_many :tests, dependent: :destroy
+
+  validates :title, presence: true
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -6,8 +6,12 @@ class Test < ApplicationRecord
   has_many :passing_tests, dependent: :destroy
   has_many :users, through: :passing_tests
 
-  def self.tests_by_category(category_title)
-    #joins("JOIN categories ON tests.category_id = categories.id").where("categories.title = ?", category_title).order(id: :desc).pluck(:title)
-    joins(:category).where({ category: { title: category_title } }).order(id: :desc).pluck(:title)
-  end
+  scope :easy_level, -> { where(level: 0..1) }
+  scope :mid_level, -> { where(level: 2..4) }
+  scope :hard_level, -> { where(level: 5..Float::INFINITY) }
+  scope :tests_by_category, ->(category_title) { joins(:category).where(category: { title: category_title }).order(id: :desc).pluck(:title) }
+
+  validates :title, uniqueness: { scope: :level }
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :title, presence: true
 end
