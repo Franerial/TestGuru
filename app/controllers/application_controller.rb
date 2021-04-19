51ecedd430e0eb4e2f5f4042b9f17b560331d805
Franerial <brugunt@gmail.com>
2021-04-19 03:27:41 +0300

    add welcome flash message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,14 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
 
   def after_sign_in_path_for(resource)
      if current_user.is_a?(Admin)
-      admin_tests_path
+       admin_tests_path
      else
-      tests_path
+       tests_path
      end
   end
 end
