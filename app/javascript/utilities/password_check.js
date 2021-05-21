c858e3abaaf26e3ae7f276d821f8bfbac56a9301
Franerial <brugunt@gmail.com>
2021-05-21 03:09:11 +0300

    lesson 15 fix 2

--- app/javascript/utilities/password_check.js
+++ app/javascript/utilities/password_check.js
@@ -1,37 +1,6 @@
-class PasswordConfirmation {
-    constructor(form) {
-        this.form = form
-        this.password = form.elements.password
-        this.confirm_password = form.elements.confirm_password
-
-        this.setup()
-    }
-
-    setup() {
-        this.form.addEventListener('input', event => {
-            if (this.confirm_password.value != '') this.checkPasswords()
-            else this.resetDefaultStyle()
-        })
-    }
-
-    checkPasswords() {
-        if (this.password.value == this.confirm_password.value) {
-            this.password.style.borderColor = 'green'
-            this.confirm_password.style.borderColor = 'green'
-        } else {
-            this.password.style.borderColor = 'red'
-            this.confirm_password.style.borderColor = 'red'
-        }
-    }
-
-    resetDefaultStyle() {
-        this.password.style.borderColor = '#ced4da'
-        this.confirm_password.style.borderColor = '#ced4da'
-    }
-
-}
+import { PasswordConfirmation } from './PasswordConfirmation.js'
 
 document.addEventListener('turbolinks:load', function () {
-    const reg_form = document.querySelector('.new_user')
-    if (reg_form) new PasswordConfirmation(reg_form)
+  const reg_form = document.querySelector('.new_user')
+  if (reg_form.elements.confirm_password) new PasswordConfirmation(reg_form)
 })
--- app/javascript/utilities/sorting.js
+++ app/javascript/utilities/sorting.js
@@ -1,41 +1,10 @@
-document.addEventListener('turbolinks:load', function () {
-    var control = document.querySelector('.sort-by-title')
+import { SortedTable } from './SortedTable.js'
 
-    if (control) control.addEventListener('click', sortRowsByTitle)
+document.addEventListener('turbolinks:load', function () {
+  const table_id = "test_table"
+  const column_name = "level"
+  const table = document.getElementById(table_id)
+
+  if (table_id != "" && column_name != "" && table != null) new SortedTable(table_id, column_name)
 })
 
-function sortRowsByTitle() {
-    var table = document.querySelector('table')
-
-    // NodeList
-    var rows = table.querySelectorAll('tr')
-    var sortedRows = []
-
-    //select all table rows except the rist one which is the header
-    for (var i = 1; i < rows.length; i++) {
-        sortedRows.push(rows[i])
-    }
-
-    var arrowUp = table.querySelector('.octicon-arrow-up')
-
-    if (this.querySelector('.octicon-arrow-up').classList.contains('hide')) {
-        sortedRows.sort((rowA, rowB) => rowA.cells[0].innerHTML > rowB.cells[0].innerHTML ? 1 : -1)
-        this.querySelector('.octicon-arrow-up').classList.remove('hide')
-        this.querySelector('.octicon-arrow-down').classList.add('hide')
-    } else {
-        sortedRows.sort((rowA, rowB) => rowA.cells[0].innerHTML > rowB.cells[0].innerHTML ? -1 : 1)
-        this.querySelector('.octicon-arrow-down').classList.remove('hide')
-        this.querySelector('.octicon-arrow-up').classList.add('hide')
-    }
-
-    var sortedTable = document.createElement('table')
-
-    sortedTable.classList.add('table', 'table-bordered')
-    sortedTable.appendChild(rows[0])
-
-    for (var i = 0; i < sortedRows.length; i++) {
-        sortedTable.appendChild(sortedRows[i])
-    }
-
-    table.parentNode.replaceChild(sortedTable, table)
-}
